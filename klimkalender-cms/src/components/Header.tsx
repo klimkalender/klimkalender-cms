7de742128deb6b5dcf7fcec9137a636b55574a7d
Robert Bakker <rbakker@rocsys.com>
2025-10-12 10:49:09 +0000

    set headr

--- klimkalender-cms/src/components/Header.tsx
+++ klimkalender-cms/src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 
   return (
     <>
-      <header className="p-4 flex items-center bg-gray-800 text-white shadow-lg">
+      <header className="p-4 flex items-center bg-white-800 text-white shadow-lg">
         <button
           onClick={() => setIsOpen(true)}
           className="p-2 hover:bg-gray-700 rounded-lg transition-colors"
@@ -19,7 +19,7 @@ export default function Header() {
         <h1 className="ml-4 text-xl font-semibold">
           <Link to="/">
             <img
-              src="/tanstack-word-logo-white.svg"
+              src="/logo.png"
               alt="TanStack Logo"
               className="h-10"
             />
