8f65933dbb6fbfb786fa298211f820fd0f10e337
Robert Bakker <rbakker@rocsys.com>
2025-10-12 15:23:50 +0000

    fix menu

--- klimkalender-cms/src/routes/__root.tsx
+++ klimkalender-cms/src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 import type { AuthContext } from '../auth'
+import Header from '@/components/Header'
 
 interface MyRouterContext {
   auth: AuthContext
@@ -11,6 +12,7 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
+      <Header />
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" initialIsOpen={false} />
     </>
--- klimkalender-cms/src/routes/index.tsx
+++ klimkalender-cms/src/routes/index.tsx
@@ -1,12 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
 import * as React from 'react'
 import { Link } from '@tanstack/react-router'
+import { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
 })
 
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 function HomeComponent() {
+   const [instruments, setInstruments] = useState<any[]>([]);
+
+  useEffect(() => {
+    getInstruments();
+  }, []);
+
+  async function getInstruments() {
+    const { data } = await supabase.from("instruments").select();
+    if (data) setInstruments(data);
+  }
+
   return (
     <div className="p-2 grid gap-2">
       <h1 className="text-xl">Welcome!</h1>
@@ -42,6 +57,14 @@ function HomeComponent() {
           </Link>
         </li>
       </ol>
+       <div>
+      <h1>Instruments</h1>
+      <ul>
+        {instruments.map((instrument) => (
+          <li key={instrument.name}>{instrument.name}</li>
+        ))}
+      </ul>
+    </div>
     </div>
   )
 }
